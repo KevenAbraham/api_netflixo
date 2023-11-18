e1b00b0d7aced2b2b2c61b6182640215a735ffad
Keven Abraham de Souza <119967485+KevenAbraham@users.noreply.github.com>
2023-11-18 00:57:54 +0000

    voltando as origens

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,42 +17,26 @@ router.get('/:id', getUser, (req, res) => {
   res.json(res.user);
 });
 
-// Rota para criar um novo usuário ou fazer login
+// Rota para criar um novo user
 router.post('/', async (req, res) => {
-  if (req.body.action === 'register') {
-    // Se a ação for "register", cria um novo usuário
-    try {
-      // Verifica se o e-mail já está cadastrado
-      const existingUser = await User.findOne({ email: req.body.email });
-      if (existingUser) {
-        return res.status(400).json({ message: 'E-mail já cadastrado. Por favor, escolha outro.' });
-      }
+  // Verifica se o e-mail já está cadastrado
+  const existingUser = await User.findOne({ email: req.body.email });
+  if (existingUser) {
+    return res.status(400).json({ message: 'E-mail já cadastrado. Por favor, escolha outro.' });
+  }
 
-      const user = new User({
-        nome: req.body.nome,
-        email: req.body.email,
-        senha: req.body.senha,
-      });
+  // Cria um novo usuário se o e-mail não estiver cadastrado
+  const user = new User({
+    nome: req.body.nome,
+    email: req.body.email,
+    senha: req.body.senha,
+  });
 
-      const newUser = await user.save();
-      res.status(201).json(newUser);
-    } catch (err) {
-      res.status(400).json({ message: err.message });
-    }
-  } else if (req.body.action === 'login') {
-    // Se a ação for "login", verifica as credenciais e faz login
-    try {
-      const user = await User.findOne({ email: req.body.email });
-      if (user && user.senha === req.body.senha) {
-        res.status(200).json(user);
-      } else {
-        res.status(401).json({ message: 'Credenciais ERRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRRO' });
-      }
-    } catch (err) {
-      res.status(500).json({ message: err.message });
-    }
-  } else {
-    res.status(400).json({ message: 'Ação inválida' });
+  try {
+    const newUser = await user.save();
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
   }
 });
 
